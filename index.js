84af7a035a83071b01e403ce473f9eaf43f64942
BenhardSIM <benhard.simanullang.bs@gmail.com>
2018-05-02 12:51:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 Function shout(string){
+<<<<<<< HEAD
   return string.toUpperCase()
 }
 Function whisper(string){
   return string.toLowerCase()
+=======
+  string
+>>>>>>> 5f6edeeadaf51d10efca3eab624e6c1e09f89243
 }
